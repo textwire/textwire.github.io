35c912d7cf01fdd5a8bba22a011805b865494d5a
SerhiiCho <serhiicho@protonmail.com>
2024-01-09 16:20:53 +0200

    Updates

---
+++ assets/index.html-tHiBZXOh.js
@@ -0,0 +1,1 @@
+import{_ as t,o,c as n,a as e}from"./app-hl-ZqicY.js";const s={},c=e("blockquote",null,[e("p",null,"WARNING! Textwire is still in development and is not ready for production use.")],-1),l=[c];function r(_,i){return o(),n("div",null,l)}const d=t(s,[["render",r],["__file","index.html.vue"]]);export{d as default};
