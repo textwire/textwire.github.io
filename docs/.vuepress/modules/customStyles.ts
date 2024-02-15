9b691c08789256ed2f133f91ba300e7caf64dc87
SerhiiCho <serhiicho@protonmail.com>
2024-02-15 13:01:57 +0200

    Change brand color

--- docs/.vuepress/modules/customStyles.ts
+++ docs/.vuepress/modules/customStyles.ts
@@ -1,4 +1,10 @@
 export default `
+    :root {
+        --c-text-accent: #cda918;
+        --c-brand: #eac11c !important;
+        --c-brand-light: #ecca42 !important;
+    }
+
     a.route-link-active {
         margin-bottom: -2px;
         border-bottom: 2px solid var(--c-text-accent);
