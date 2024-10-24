6202863d0af3f9f3b853d2b946ce19fc36bffe42
SerhiiCho <serhiicho@protonmail.com>
2024-10-24 11:22:18 +0300

    Updates

---
+++ assets/js/3a2db09e.9844c85d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[8121],{8070:e=>{e.exports=JSON.parse('{"tags":[{"label":"Version Release Notes","permalink":"/blog/tags/release","description":"The latest version release notes","count":1},{"label":"Story","permalink":"/blog/tags/story","description":"The story behind Textwire","count":1}]}')}}]);
