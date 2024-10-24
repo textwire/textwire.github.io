0624093d25678446b08e91d638b96a6b899da21c
SerhiiCho <serhiicho@protonmail.com>
2024-10-24 15:38:52 +0300

    Remove search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -109,32 +109,6 @@ const config: Config = {
             theme: prismThemes.github,
             darkTheme: prismThemes.oneDark,
         },
-        algolia: {
-            appId: '0JTU9QLN4V',
-            apiKey: '505627364f4ecc995c30aecef053f7b9',
-            indexName: 'Textwire',
-
-            // Optional: see doc section below
-            contextualSearch: true,
-
-            // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-            // externalUrlRegex: 'external\\.com|domain\\.com',
-
-            // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-            // replaceSearchResultPathname: {
-            //     from: '/docs/', // or as RegExp: /\/docs\//
-            //     to: '/',
-            // },
-
-            // Optional: Algolia search parameters
-            // searchParameters: {},
-
-            // Optional: path for search page that enabled by default (`false` to disable it)
-            // searchPagePath: 'search',
-
-            // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
-            insights: false,
-        },
     } satisfies Preset.ThemeConfig,
 }
 
