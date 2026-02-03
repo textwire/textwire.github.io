af7153c9d8cc76d05bfcac0b98ebcfec639b0d2c
SerhiiCho <serhiicho@protonmail.com>
2026-02-03 11:18:35 +0200

    refactor: community page

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -24,6 +24,13 @@ export default function Community(): React.ReactElement {
                     </p>
                 </section>
 
+                <section className="first-issue-contributor">
+                    <h2>First Feature Suggestion</h2>
+                    <p>
+                        A special thanks to <a href="https://github.com/joeyjurjens" target="_blank" rel="noopener noreferrer">@joeyjurjens</a> for opening the first issue and suggesting the custom functions feature in <a href="https://github.com/textwire/textwire/issues/18" target="_blank" rel="noopener noreferrer">this</a> GitHub issue! Your contribution helped to shape Textwire.
+                    </p>
+                </section>
+
                 <section className="early-stargazers">
                     <h2>First People to Star Textwire</h2>
 
