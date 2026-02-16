b5785d7356a658b07f25811c05893c19850bc23b
SerhiiCho <serhiicho@protonmail.com>
2026-02-16 15:02:20 +0200

    refactor: config

---
+++ docs/.vitepress/sidebars/sidebarV2.ts
@@ -0,0 +1,99 @@
+export const sidebarV2 = [
+    { text: 'Introduction', link: '/v2/' },
+    { text: 'Get Started', link: '/v2/get-started' },
+    {
+        text: 'Guides',
+        items: [
+            {
+                text: 'Usage with Templates',
+                link: '/v2/guides/template-usage',
+            },
+            {
+                text: 'Evaluating Strings',
+                link: '/v2/guides/eval-string',
+            },
+            {
+                text: 'Evaluating Files',
+                link: '/v2/guides/eval-file',
+            },
+            {
+                text: 'Custom Functions',
+                link: '/v2/guides/custom-functions',
+            },
+            {
+                text: 'Error Handling',
+                link: '/v2/guides/error-handling',
+            },
+            {
+                text: 'Configurations',
+                link: '/v2/guides/configurations',
+            },
+            {
+                text: 'Loops Usage',
+                link: '/v2/guides/loops',
+            },
+        ],
+    },
+    {
+        text: 'Language Elements',
+        items: [
+            {
+                text: 'Syntax',
+                link: '/v2/language-elements/syntax',
+            },
+            {
+                text: 'Statements',
+                link: '/v2/language-elements/statements',
+            },
+            {
+                text: 'Expressions',
+                link: '/v2/language-elements/expressions',
+            },
+            {
+                text: 'Literals',
+                link: '/v2/language-elements/literals',
+            },
+            {
+                text: 'Other Information',
+                link: '/v2/language-elements/other',
+            },
+        ],
+    },
+    {
+        text: 'Functions',
+        items: [
+            {
+                text: 'Functions Guide',
+                link: '/v2/functions/guide',
+            },
+            {
+                text: 'Integer Functions',
+                link: '/v2/functions/int',
+            },
+            {
+                text: 'Float Functions',
+                link: '/v2/functions/float',
+            },
+            {
+                text: 'Boolean Functions',
+                link: '/v2/functions/bool',
+            },
+            {
+                text: 'Array Functions',
+                link: '/v2/functions/arr',
+            },
+            {
+                text: 'String Functions',
+                link: '/v2/functions/str',
+            },
+        ],
+    },
+    {
+        text: 'FAQ',
+        link: '/v2/faq',
+    },
+    {
+        text: 'Upgrade Guide',
+        link: '/v2/upgrade',
+    },
+]
